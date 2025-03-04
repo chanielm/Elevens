8a2f053b5d43e08f222c479d5aa5683848d0f305
chanielm <chanieljm@gmail.com>
2025-03-04 15:18:44 -0500

    Card implemented

---
+++ Rank.cs
@@ -0,0 +1,34 @@
+/*
+Enumeration: Rank
+    ATTRIBUTE   VALUE
+    Ace         0
+    Two         1
+    Three       2
+    Four        3
+    Five        4
+    Six         5
+    Seven       6
+    Eight       7
+    Nine        8
+    Ten         9
+    Jack        10
+    Queen       11
+    King        12
+
+*/
+
+enum Rank {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King
+}
---
+++ Suit.cs
@@ -0,0 +1,15 @@
+/*
+Enumeration: Suit
+    ATTRIBUTE   VALUE
+    Spades      0
+    Clubs       1
+    Diamonds    2
+    Hearts      3
+*/
+
+enum Suit {
+    Spades,
+    Clubs,
+    Diamonds,
+    Hearts
+}
